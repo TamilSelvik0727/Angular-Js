ea4e7ba510806b724da76e499bf25b6c0774de9c
TamilSelvik0727 <111735176+TamilSelvik0727@users.noreply.github.com>
2023-03-27 17:33:18 +0530

    Add files via upload

---
+++ Angularjs/resultController.js
@@ -0,0 +1,8 @@
+// define controller
+angular.module('myApp',[]).controller('resultCtrl',function($scope){
+
+    $scope.result=[{name:'Tamil',grade:'B+'},{name:'buddy',grade:'A+'},{name:'selvi',grade:'A+'}];
+
+    
+
+});
